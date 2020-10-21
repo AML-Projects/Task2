74b855dcd43ba8ad5c4661a82f2a0ebc7286a6c5
braunjon <25931090+braunjon@users.noreply.github.com>
2020-10-21 20:06:45 +0200

    Small refactoring

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -6,38 +6,44 @@ from imblearn.under_sampling import AllKNN, ClusterCentroids, RandomUnderSampler
 class DataSampling:
     def __init__(self, over_sampling_method):
         self.over_sampling_method = over_sampling_method
-        print(self.over_sampling_method)
+        print("\nData Sampling:", self.over_sampling_method)
 
     def sampling(self, X, y):
         # oversampling
         if self.over_sampling_method == "random":
-            return RandomOverSampler(random_state=41).fit_resample(X, y)
+            sampler = RandomOverSampler(random_state=41)
 
         if self.over_sampling_method == "SMOTE":
-            return SMOTE(n_jobs=-1, random_state=41).fit_resample(X, y)
+            sampler = SMOTE(n_jobs=-1, random_state=41)
 
         if self.over_sampling_method == "ADASYN":
-            return ADASYN(n_jobs=-1, random_state=41).fit_resample(X, y)
+            sampler = ADASYN(n_jobs=-1, random_state=41)
 
         # under sampling
         if self.over_sampling_method == "RandomUnderSampler":
-            return RandomUnderSampler(random_state=0).fit_resample(X, y)
+            sampler = RandomUnderSampler(random_state=0)
 
         if self.over_sampling_method == "ClusterCentroids":
-            return ClusterCentroids(random_state=41).fit_resample(X, y)
+            sampler = ClusterCentroids(random_state=41)
 
         if self.over_sampling_method == "NearMiss":
-            return NearMiss().fit_resample(X, y)
+            sampler = NearMiss().fit_resample(X, y)
 
         if self.over_sampling_method == "AllKNN":
-            return AllKNN(n_jobs=-1).fit_resample(X, y)
+            sampler = AllKNN(sampling_strategy='not minority', n_jobs=-1)
 
         # over and under sampling
         if self.over_sampling_method == "SMOTEENN":
-            return SMOTEENN(n_jobs=-1, random_state=41).fit_resample(X, y)
+            sampler = SMOTEENN(n_jobs=-1, random_state=41)
 
         if self.over_sampling_method == "SMOTETomek":
-            return SMOTETomek(n_jobs=-1, random_state=41).fit_resample(X, y)
+            sampler = SMOTETomek(n_jobs=-1, random_state=41)
+
+        x_sampled, y_sampled = sampler.fit_resample(X, y)
+
+        print("\nResampled shape\n", x_sampled.shape)
+
+        return x_sampled, y_sampled
 
     def fit_resample(self, X, y):
         if self.over_sampling_method == "None":
