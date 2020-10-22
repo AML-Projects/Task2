de62093e2dfa77e2d95d403b8d0588dc040af01b
braunjon <25931090+braunjon@users.noreply.github.com>
2020-10-22 17:22:21 +0200

    Added feature selector

---
+++ feature_selector.py
@@ -0,0 +1,23 @@
+from sklearn.base import TransformerMixin
+from sklearn.kernel_approximation import Nystroem
+
+
+class FeatureSelector(TransformerMixin):
+    def __init__(self, method="None"):
+        self.method = method
+        self.feature_map_nystroem = Nystroem(
+            gamma=None,
+            n_components=200,
+            random_state=41)
+
+    def fit(self, X, y):
+        if self.method == "Nystroem":
+            return self.feature_map_nystroem.fit(X, y)
+        else:
+            return self
+
+    def transform(self, X):
+        if self.method == "Nystroem":
+            return self.feature_map_nystroem.transform(X)
+        else:
+            return X
