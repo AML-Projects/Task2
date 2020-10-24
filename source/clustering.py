943150d269f46956977aa52bdd79a179f99df623
braunjon <25931090+braunjon@users.noreply.github.com>
2020-10-24 15:46:07 +0200

    Added visualization of different feature extraction methods

--- source/clustering.py
+++ source/clustering.py
@@ -1,7 +1,11 @@
 import matplotlib.pyplot as plt
-from sklearn.cluster import KMeans, AgglomerativeClustering, Birch
-from sklearn.decomposition import PCA
+import tensorflow
+from sklearn.cluster import KMeans, AgglomerativeClustering, Birch, FeatureAgglomeration
+from sklearn.cross_decomposition import PLSCanonical, CCA, PLSSVD, PLSRegression
+from sklearn.decomposition import PCA, FastICA
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.manifold import LocallyLinearEmbedding, TSNE, MDS, Isomap
+from sklearn.preprocessing import StandardScaler
 
 
 def visualize(x, y):
@@ -13,29 +17,40 @@ def visualize(x, y):
     :return:
     """
 
-    pca = PCA(n_components=3)
-    principal_components = pca.fit_transform(x, y)
+    # make sure data is scaled
+    scaler = StandardScaler()
+    x = scaler.fit_transform(x, y)
 
-    """
-    explained_variance = pca.explained_variance_ratio_
+    # cross decomposition
+    cross_decomposers = {
+        "PLSCanonical": PLSCanonical(n_components=2),  # 2 because the 3rd component is just zero
+        "CCA": CCA(n_components=2),  # 2 because the 3rd component is just zero
+        "PLSSVD": PLSSVD(n_components=3),
+        "PLSRegression": PLSRegression(n_components=3),
+    }
 
-    print("pca explained variance", explained_variance)
+    for extractor_name, extractor in cross_decomposers.items():
+        x_components, y_component = extractor.fit_transform(x, tensorflow.keras.utils.to_categorical(y, 3))
+        plot_true_labels(x, y, x_components, extractor_name)
 
-    principal_components_T = principal_components.T
-    for i in range(0, nr_components):
-        fig, ax = plt.subplots()
-        plt.scatter(principal_components_T[i], y.astype(int))
-        ax.set(xlabel='pc' + str(i + 1), ylabel='class', title='principle component ' + str(i + 1) + ' vs target label')
-        ax.grid()
-        plt.show()
-    """
-
-    # lda returns max 2 components
-    lda = LinearDiscriminantAnalysis(n_components=2)
-    lda_components = lda.fit_transform(x, y)
-
-    # fa = FeatureAgglomeration(n_clusters=3, linkage='ward')
-    # feature_agglo = fa.fit_transform(x, y)
+    feature_extractors = {
+        # Discriminant Analysis
+        # https://scikit-learn.org/stable/modules/classes.html#module-sklearn.discriminant_analysis
+        "LDA": LinearDiscriminantAnalysis(n_components=2),
+        # decomposition
+        # https://scikit-learn.org/stable/modules/classes.html#module-sklearn.decomposition
+        "PCA": PCA(n_components=3),
+        "FastICA": FastICA(n_components=3, random_state=41),
+        # Manifold Learning
+        # https://scikit-learn.org/stable/modules/classes.html#module-sklearn.manifold
+        "Isomap": Isomap(n_components=3, n_jobs=-1),
+        "LocallyLinearEmbedding": LocallyLinearEmbedding(n_components=3, n_jobs=-1, random_state=41),
+        "MDS": MDS(n_components=3, n_jobs=-1, random_state=41),
+        "TSNE": TSNE(n_components=3, n_jobs=-1, random_state=41),
+        # Clustering
+        # https://scikit-learn.org/stable/modules/classes.html#module-sklearn.cluster
+        "FeatureAgglomeration": FeatureAgglomeration(n_clusters=3, linkage='ward')
+    }
 
     cluster_dict = {
         "KMeans": KMeans(n_clusters=3, random_state=41),
@@ -43,17 +58,36 @@ def visualize(x, y):
         # linkage : {'ward', 'complete', 'average', 'single'}
         "AgglomerativeClustering": AgglomerativeClustering(n_clusters=3, linkage='ward'),
     }
+    for extractor_name, extractor in feature_extractors.items():
+        components = extractor.fit_transform(x, y)
+        plot_true_labels(x, y, components, extractor_name)
 
+    plt.show()
+
+    """
     for name, clusterer in cluster_dict.items():
-        plot_predicted_clusters(x, y, principal_components, clusterer, "PCA - " + name)
-        plot_predicted_clusters(x, y, lda_components, clusterer, "LDA - " + name)
-        # plot_predicted_clusters(x, y, feature_agglo, clusterer, "FeatureAgglomeration - " + name)
-        plt.show()
-
-    return principal_components
+        plot_predicted_clusters(x, y, components, clusterer, extractor_name + " - " + name)
+    """
 
 
-def plot_predicted_clusters(x, y, components, clusterer, title="", interactive=False):
+def plot_true_labels(x, y, components, title="", save_img=True, interactive=False):
+    if interactive:
+        # pip install PyQt5
+        import matplotlib as mpl
+        mpl.use("Qt5Agg")
+
+    fig = plt.figure(figsize=(12, 12))
+    fig.suptitle(title, fontsize=20)
+    fig.set_dpi(150)
+
+    # plot ture values
+    y_pred = y.values.T
+    subplot_components(components, 111, "True labels", y_pred)
+    if save_img:
+        plt.savefig(fname="./img/extraction/" + title + ".png")
+
+
+def plot_predicted_clusters(x, y, components, clusterer, title="", interactive=True):
     if interactive:
         # pip install PyQt5
         import matplotlib as mpl
@@ -82,9 +116,12 @@ def subplot_components(components, plot_nr, plot_title, y_pred):
         ax = plt.subplot(plot_nr, projection='3d')
         ax.autoscale(tight=True)
         ax.scatter(components[:, 0], components[:, 1], components[:, 2], c=y_pred)
+        ax.elev = 65.0
+        ax.azim = 45.0
+        ax.set_zlabel('component 2')
     else:
         plt.subplot(plot_nr)
         plt.scatter(components[:, 0], components[:, 1], c=y_pred)
-    plt.xlabel('comp 0')
-    plt.ylabel('comp 1')
+    plt.xlabel('component 0')
+    plt.ylabel('component 1')
     plt.title(plot_title)
