562bdb6b776fc99864d2a4c99f72ee0854393144
braunjon <25931090+braunjon@users.noreply.github.com>
2020-10-28 21:27:15 +0100

    Make data split according to Agglomerative Clustering

--- helpers/data_splitter.py
+++ helpers/data_splitter.py
@@ -1,6 +1,7 @@
 import os
 
 import pandas as pd
+from sklearn.cluster import AgglomerativeClustering
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import RobustScaler
 
@@ -9,29 +10,26 @@ from logcreator.logcreator import Logcreator
 
 
 class DataSplitter:
-    def __init__(self):
+    def __init__(self, method="cluster"):
+        self.method = method
         pass
 
-    def split_data(self, x, y=None, log_text="", boundary=0):
+    def split_data(self, x, y=None, log_text="", pca_boundary=0):
         """
         Split data according to PCA component 0 according to the supplied boundary.
         :param x:
         :param y:
-        :param boundary:
+        :param log_text:
+        :param pca_boundary:
         :return:
         """
-        pca = PCA(n_components=1)
-
-        # scale x
+        # scale x only for splitting
         scaler = RobustScaler()
         x_scaled = scaler.fit_transform(x)
 
-        visualize_true_labels(x, y_train, "Before split - " + log_text)
+        visualize_true_labels(x, y_train, "Before split - " + log_text, interactive=False)
 
-        components = pca.fit_transform(x_scaled)
-        components = pd.DataFrame(components)
-
-        remove_rows = (components > boundary)
+        remove_rows = self.do_split(pca_boundary, x_scaled, log_text)
 
         self.remove_idx1 = remove_rows.index[remove_rows[0] == False].tolist()
         self.remove_idx2 = remove_rows.index[remove_rows[0] == True].tolist()
@@ -52,6 +50,24 @@ class DataSplitter:
         visualize_true_labels(x2, y_true=None, title="Split 2 - " + log_text)
 
         return x1, x2
+
+    def do_split(self, boundary, x, log_text=""):
+        if self.method == "cluster":
+            clusterer = AgglomerativeClustering(n_clusters=2, linkage='ward')
+
+            y_cluster_pred = clusterer.fit_predict(x)
+
+            visualize_true_labels(x, y_cluster_pred, log_text + " Cluster split boundary", interactive=False)
+
+            remove_rows = (pd.DataFrame(y_cluster_pred) == 0)
+        else:
+            pca = PCA(n_components=1)
+            components = pca.fit_transform(x)
+            components = pd.DataFrame(components)
+
+            remove_rows = (components > boundary)
+
+        return remove_rows
 
     def combine_split(self, x1, x2):
         """
@@ -83,25 +99,28 @@ if __name__ == '__main__':
     Logcreator.info("\nBefore split - Train\n", y_train.groupby("y")["y"].count().values)
 
     # split data
+    method = "cluster"
     boundary = 5
 
-    splitter_train = DataSplitter()
+    splitter_train = DataSplitter(method=method)
     x1_train, y1_train, x2_train, y2_train = splitter_train.split_data(x_train, y_train,
                                                                        log_text="Train",
-                                                                       boundary=boundary)
+                                                                       pca_boundary=boundary)
 
-    splitter_test = DataSplitter()
-    x1_test, x2_test = splitter_test.split_data(x_test, log_text="Test", boundary=boundary)
+    splitter_test = DataSplitter(method=method)
+    x1_test, x2_test = splitter_test.split_data(x_test, log_text="Test", pca_boundary=boundary)
 
     # after split
     Logcreator.info("\nSplit 1 - Train\n", y1_train.groupby("y")["y"].count().values)
     Logcreator.info("\nSplit 2 - Train\n", y2_train.groupby("y")["y"].count().values)
 
     # save to file
-    save_to_file(x1_train, 'x1_train.csv')
-    save_to_file(y1_train, 'y1_train.csv')
-    save_to_file(x2_train, 'x2_train.csv')
-    save_to_file(y2_train, 'y2_train.csv')
+    save_active = True
+    if save_active:
+        save_to_file(x1_train, 'x1_train.csv')
+        save_to_file(y1_train, 'y1_train.csv')
+        save_to_file(x2_train, 'x2_train.csv')
+        save_to_file(y2_train, 'y2_train.csv')
 
-    save_to_file(x1_test, 'x1_test.csv')
-    save_to_file(x2_test, 'x2_test.csv')
+        save_to_file(x1_test, 'x1_test.csv')
+        save_to_file(x2_test, 'x2_test.csv')
