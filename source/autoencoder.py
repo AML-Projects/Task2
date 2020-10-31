d1d5d58c7ff609db63385bc72dc88a865aa3a626
braunjon <25931090+braunjon@users.noreply.github.com>
2020-10-31 22:02:27 +0100

    Added pretrained auto encoder

--- source/autoencoder.py
+++ source/autoencoder.py
@@ -1,3 +1,4 @@
+import keras
 import numpy as np
 import pandas
 from keras.layers import Dense, Input
@@ -20,12 +21,20 @@ from logcreator.logcreator import Logcreator
 
 
 class AutoEncoder(BaseEstimator, TransformerMixin):
-    def __init__(self, add_noise=False, encoded_size=64, scaling_on=True, use_sample_weight=True, verbose=1):
+    def __init__(self, add_noise=False, encoded_size=64, scaling_on=True, use_sample_weight=True,
+                 load_model_path="", verbose=1):
         self.add_noise = add_noise
         self.encoded_size = encoded_size
         self.scaling_on = scaling_on
         self.use_sample_weight = use_sample_weight
         self.verbose = verbose
+        self.encoder = None
+        try:
+            self.encoder = keras.models.load_model(load_model_path)
+            Logcreator.info("Encoder model loaded:", load_model_path)
+        except:
+            Logcreator.info("No model loaded")
+
         if self.verbose == 1:
             Logcreator.info("\nAuto encoder:")
             Logcreator.info("add_noise =", add_noise)
@@ -48,6 +57,10 @@ class AutoEncoder(BaseEstimator, TransformerMixin):
         :param X:
         :return:
         """
+        if self.encoder is not None:
+            # encoder was loaded from file
+            return self
+
         input_size = X.shape[1]
         hidden_size = 256
         encoded_size = self.encoded_size
@@ -56,7 +69,7 @@ class AutoEncoder(BaseEstimator, TransformerMixin):
 
         # add noise to input
         if self.add_noise:
-            noise_factor = 0.3
+            noise_factor = 0.01
             x_train_noisy = X + noise_factor * np.random.normal(size=X.shape)
             x_train_noisy = np.clip(x_train_noisy, 0.0, 1.0)
             X = x_train_noisy
@@ -67,7 +80,7 @@ class AutoEncoder(BaseEstimator, TransformerMixin):
         hidden_2 = Dense(hidden_size, activation='elu', activity_regularizer=l1_l2(10e-6, 10e-6))(encoded)
         decoded = Dense(input_size, activation='sigmoid')(hidden_2)
 
-        autoencoder = Model(input_img, decoded)
+        auto_encoder = Model(input_img, decoded)
 
         # This model maps an input to its encoded representation
         self.encoder = Model(input_img, encoded)
@@ -75,10 +88,10 @@ class AutoEncoder(BaseEstimator, TransformerMixin):
         optimizer = Adamax(learning_rate=0.0005)
         # optimizer = Nadam(learning_rate=0.0001)
 
-        autoencoder.compile(optimizer=optimizer, loss='mean_squared_error',
-                            metrics=['binary_crossentropy', 'mean_absolute_error'])
+        auto_encoder.compile(optimizer=optimizer, loss='mean_squared_error',
+                             metrics=['binary_crossentropy', 'mean_absolute_error'])
 
-        es = EarlyStopping(monitor='val_loss', mode='min', verbose=self.verbose, patience=8)
+        es = EarlyStopping(monitor='val_loss', mode='min', verbose=self.verbose, patience=16)
 
         if self.use_sample_weight:
             classes = np.unique(y['y'])
@@ -89,16 +102,16 @@ class AutoEncoder(BaseEstimator, TransformerMixin):
             for i in range(0, len(class_weights)):
                 weights.loc[weights.y == i] = class_weights[i]
 
-            history = autoencoder.fit(X, X, epochs=200, batch_size=10, callbacks=[es],
-                                      sample_weight=weights,
-                                      shuffle=True,
-                                      validation_split=0.2,
-                                      verbose=self.verbose)
+            history = auto_encoder.fit(X, X, epochs=512, batch_size=10, callbacks=[es],
+                                       sample_weight=weights,
+                                       shuffle=True,
+                                       validation_split=0.05,
+                                       verbose=self.verbose)
         else:
-            history = autoencoder.fit(X, X, epochs=200, batch_size=10, callbacks=[es],
-                                      shuffle=True,
-                                      validation_split=0.2,
-                                      verbose=self.verbose)
+            history = auto_encoder.fit(X, X, epochs=512, batch_size=10, callbacks=[es],
+                                       shuffle=True,
+                                       validation_split=0.05,
+                                       verbose=self.verbose)
 
         if self.verbose == 1:
             Logcreator.info("Auto encoder parameter:", history.params)
@@ -112,19 +125,61 @@ class AutoEncoder(BaseEstimator, TransformerMixin):
         # evaluate performance
         evaluate = False
         if evaluate:
-            clf = SVC(class_weight='balanced')
-            clf.fit(self.encoder.predict(X), y)
-            y_pred = clf.predict(self.encoder.predict(X))
-
-            evaluate_bas(y_true=y, y_predicted=y_pred, text="Auto Encoder with default SVC on Train")
-
-            visualize_prediction(X, y, y_pred, "Auto Encoded")
+            evaluate_encoding(self.encoder.predict(X), y)
 
         return self
 
     def transform(self, X, *_):
         X = self.scale_input(X)
         return self.encoder.predict(X)
+
+
+def evaluate_encoding(X, y):
+    """
+    Evaluate encoding with a classifier.
+    :param X:
+    :param y:
+    :return:
+    """
+    clf = SVC(class_weight='balanced')
+    clf.fit(X, y)
+    y_pred = clf.predict(X)
+    evaluate_bas(y_true=y, y_predicted=y_pred, text="Auto Encoder with default SVC on fit data")
+    visualize_prediction(X, y, y_pred, "Auto Encoded")
+
+
+def train_auto_encoder(on_all_data=False):
+    """
+    Train the auto encoder on data and save the model
+    """
+    x_train = pandas.read_csv("../data/X_train.csv", index_col=0)
+    y_train = pandas.read_csv("../data/y_train.csv", index_col=0)
+    x_test = pandas.read_csv("../data/X_test.csv", index_col=0)
+
+    if on_all_data:
+        ae = AutoEncoder(add_noise=True, encoded_size=128, scaling_on=True,
+                         use_sample_weight=False,
+                         load_model_path="",
+                         verbose=1)
+
+        ae.fit(x_train.append(x_test).reset_index(drop=True), None)
+
+        # save the encoder
+        ae.encoder.save('../model_save/encoder_all_denoise_128')
+    else:
+        # on training data
+        ae = AutoEncoder(add_noise=True, encoded_size=128, scaling_on=True,
+                         # For the training data we know the weight per sample!
+                         use_sample_weight=True,
+                         load_model_path="",
+                         verbose=1)
+
+        ae.fit(x_train, y_train)
+
+        ae.encoder.save('../model_save/encoder_train_denoise_128')
+
+    # evaluate encoding with classifier
+    evaluate_encoding(ae.transform(x_train), y_train)
 
 
 def grid_search_auto_encoder():
@@ -178,5 +233,8 @@ if __name__ == "__main__":
     pandas.set_option('display.width', None)
     pandas.set_option('display.max_colwidth', None)
 
-    # check_estimator(AutoEncoder)
-    grid_search_auto_encoder()
+    train = True
+    if train:
+        train_auto_encoder()
+    else:
+        grid_search_auto_encoder()
